e40bc1388c5af2344c49a35d2917d97bcb2cbd2b
Brahim Filali <brahimfilali758@gmail.com>
2020-10-29 04:32:53 +0100

    User authentification feature added succefully

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo , ValidationError
+from flaskblog.models import User , Post
 
 
 class RegistrationForm(FlaskForm):
@@ -13,10 +14,22 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self , username) :
+        user = User.query.filter_by(username=username.data).first()
+        if user :
+            raise ValidationError('Username is already taken , PLease choose another one !')
+
+    def validate_email(self , email) :
+        user = User.query.filter_by(email=email.data).first()
+        if user :
+            raise ValidationError('Email is already taken , PLease choose another one !')
+
+
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',
-                        validators=[DataRequired()])
+    email = StringField('Email',
+                        validators=[DataRequired() ,Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
