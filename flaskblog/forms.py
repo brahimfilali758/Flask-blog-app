f1abf56461bac65cc040fd1824201fcaa25c7ba8
Brahim Filali <brahimfilali758@gmail.com>
2020-11-01 19:55:28 +0100

    Creating, Updating, and Deleting posts features

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -68,3 +68,11 @@ class CreatePostForm(FlaskForm):
                         validators=[DataRequired()] , render_kw={"rows": 10, "cols": 11})
 
     submit = SubmitField('Submit Your Post')
+
+
+class UpdatePostForm(FlaskForm):
+    title = StringField('Title of your post : ' , validators=[DataRequired()])
+    content = TextAreaField('Express yourselef :',
+                        validators=[DataRequired()] , render_kw={"rows": 10, "cols": 11})
+
+    submit = SubmitField('Submit Your Post')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,18 +2,16 @@ from PIL import Image
 from secrets import token_hex
 from os.path import join , splitext
 from flaskblog import app , bcrypt , db
-from flask import render_template , url_for , flash ,redirect , request
-from flaskblog.forms import RegistrationForm , LoginForm , UpdateAccountForm , CreatePostForm
+from flask import render_template , url_for , flash ,redirect , request , abort
+from flaskblog.forms import RegistrationForm , LoginForm , UpdateAccountForm , CreatePostForm ,UpdatePostForm
 from flaskblog.models import User , Post
 from flask_login import login_user , logout_user , current_user , login_required
 
 @app.route("/")
 @app.route("/home")
-@login_required
 def home():
     posts = Post.query.all()
-    image_file = url_for('static', filename=join('profile_pics',current_user.image_file))
-    return render_template('home.html' , title = "Home page" , posts = posts , image_file = image_file)
+    return render_template('home.html' , title = "Home page" , posts = posts )
 
 
 @app.route("/about")
@@ -62,7 +60,7 @@ def logout():
         return redirect(url_for('login'))
 
 
-@app.route("/account" )
+@app.route("/account")
 @login_required
 def account():
     image_file = url_for('static', filename=join('profile_pics',current_user.image_file))
@@ -120,4 +118,50 @@ def create_post():
         flash('Your post has been published succefully !' , 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html' ,title= 'Create a Post Now !', form = form)
+
+
+@app.route("/post/<int:post_id>" , methods=['GET','POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html' , title = post.id , post = post)
+
+
+
+@app.route("/post/<int:post_id>/update" , methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = UpdatePostForm()
+    if post.author != current_user :
+        abort(403)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your Post has been Updated succefully ! ' , 'success')
+        return redirect(url_for('post' , post_id = post.id))
+    elif request.method == 'GET' :
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('update_post.html' , title = 'Update Post : ' + post.title , form = form)
+
+
+
+@app.route("/my_posts")
+@login_required
+def my_posts():
+    posts = Post.query.filter_by(author = current_user).all()
+    return render_template('my_posts.html' , title = 'My Posts' , posts = posts)
+
+
+@app.route("/post/<int:post_id>/delete" , methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user :
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your Post has been deleted succefully ! ' , 'info')
+    return redirect(url_for('home'))
 
