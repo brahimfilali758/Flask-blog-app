f9a536fca5fe0af3b06e3a01095f22d3cec26435
Brahim Filali <brahimfilali758@gmail.com>
2020-11-06 01:53:50 +0100

    Password reset feature by email sending added Succefully

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv # just to load the environements variables
+
+#load the environements variables :
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='lkdfjglsdfjgljdlmfkgjdlj565'
@@ -11,5 +17,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config["MAIL_USE_SSL"] = True
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+mail = Mail(app)
+
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -76,3 +76,21 @@ class UpdatePostForm(FlaskForm):
                         validators=[DataRequired()] , render_kw={"rows": 10, "cols": 11})
 
     submit = SubmitField('Submit Your Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self , email) :
+        user = User.query.filter_by(email=email.data).first()
+        if user is None :
+            raise ValidationError('If an account with this email address exists, a password reset message will be sent shortly.')
+class ResetPasswordForm(FlaskForm):
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
